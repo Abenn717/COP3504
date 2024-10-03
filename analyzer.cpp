559a83e9492f5acfd915210871b509e1ef8f325b
Samson <zhsamson12@gmail.com>
2024-10-03 19:56:56 -0400

    Add files via upload

--- analyzer.cpp
+++ analyzer.cpp
@@ -1,52 +1,77 @@
 #include <iostream>
-#include <string>
 #include "StringData.h"
+#include <string>
 
-int linear_search(std::vector<std::string> container, std::string str) {
-    for (int i = 0; i < container.size(); i++) {
-        std::string value = container[i];
-        if(value==str) {
+int linear_search(std::vector<std::string> arr, std::string element)
+{
+    int size = arr.size()-1;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
             return i;
         }
-
     }
- return -1;
-}
 
-int binary_search(std::vector<std::string> container, std::string str) {
-    int low = 0;
-    int high = container.size() - 1;
-    int mid;
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if (container[mid] == str) {
-            return mid;
-        }
-        else if (container[mid] < str) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;}
-        }
     return -1;
 }
 
-int main(){
-    std::vector<std::string> data = getStringData();
-    std::string words[3] = {"not_here", "mzzzz", "aaaaa"};
-
-
-    for(std::string test : words) {
-        std::cout<<"Linear Search for "<<test<<std::endl;
-        std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
-        int result = linear_search(data, test);
-        std::chrono::time_point<std::chrono::system_clock> end_time = std::chrono::system_clock::now();
-        std::cout<<"Index/Result: "<<result<<" Time taken: "<<end_time-start_time<<" seconds"<<std::endl<<std::endl;
-    }
-    for(std::string test : words) {
-        std::cout<<"Binary Search for "<<test<<std::endl;
-        std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
-        int result = binary_search(data, test);
-        std::chrono::time_point<std::chrono::system_clock> end_time = std::chrono::system_clock::now();
-        std::cout<<"Index/Result: "<<result<<" Time taken: "<<end_time-start_time<<" seconds"<<std::endl<<std::endl;
+int binary_search(std::vector<std::string> bin_arr, std::string bin_element)
+{
+    int low = 0;
+    int high = bin_arr.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low)/ 2;
+        if (bin_arr[mid] == bin_element)
+        {
+            return mid;
+        }
+        else if (bin_arr[mid] < bin_element)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
         }
     }
+    return -1;
+}
+int main()
+{
+    std::vector<std::string> stringcontainer = getStringData();
+    
+    std::chrono::time_point<std::chrono::system_clock> initial = std::chrono::system_clock::now();
+    int linear_index = linear_search(stringcontainer, "not_here");
+    std::chrono::time_point<std::chrono::system_clock> final = std::chrono::system_clock::now();
+    std::cout<<"Time it took for a linear search of not_here: "<<final-initial<<" and its index is "<< linear_index<<std::endl;
+
+    initial = std::chrono::system_clock::now();
+    int binary_index = binary_search(stringcontainer, "not_here");
+    final = std::chrono::system_clock::now();
+    std::cout<<"Time it took for a binary search of not_here: "<<final-initial<<" and its index is "<< binary_index<<std::endl;
+
+    initial = std::chrono::system_clock::now();
+    linear_index = linear_search(stringcontainer, "mzzzz");
+    final = std::chrono::system_clock::now();
+    std::cout<<"Time it took for a linear search of mzzzz: "<<final-initial<<" and its index is "<< linear_index<<std::endl;
+
+    initial = std::chrono::system_clock::now();
+    binary_index = binary_search(stringcontainer, "mzzzz");
+    final = std::chrono::system_clock::now();
+    std::cout<<"Time it took for a binary search of mzzzz: "<<final-initial<<" and its index is "<< binary_index<<std::endl;
+
+    initial = std::chrono::system_clock::now();
+    linear_index = linear_search(stringcontainer, "aaaaa");
+    final = std::chrono::system_clock::now();
+    std::cout<<"Time it took for a linear search of aaaaa: "<<final-initial<<" and its index is "<< linear_index<<std::endl;
+
+    initial = std::chrono::system_clock::now();
+    binary_index = binary_search(stringcontainer, "aaaaa");
+    final = std::chrono::system_clock::now();
+    std::cout<<"Time it took for a binary search of aaaaa: "<<final-initial<<" and its index is "<< binary_index<<std::endl;
+
+    
+    return 0;
+}
